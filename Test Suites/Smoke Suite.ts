77dd80e6ce56f8e2fcad733dd0259ecafa2a4c20
lior.marom <lior.marom@192.168.86.33>
2025-05-09 09:58:25 -0400

    Committing TC and Suite

---
+++ Test Suites/Smoke Suite.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Smoke Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>5a008454-c35a-4121-881c-a7d9865b3cf7</testSuiteGuid>
+</TestSuiteEntity>
